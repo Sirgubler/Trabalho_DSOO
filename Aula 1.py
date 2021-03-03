8a9c9dee0261de93428a89846e8d83e4c428c169
Sirgubler <49541261+Sirgubler@users.noreply.github.com>
2021-03-03 15:52:46 -0300

    Add files via upload
    Base do Trabalho

---
+++ Aula 1.py
@@ -0,0 +1,65 @@
+class Livro:
+    def __init__(self, titulo, resumo, autor, protagonista, genero, faixa_etaria):
+        self.titulo = titulo
+        self.resumo = resumo
+        self.autor = autor
+        self.protagonista = protagonista
+        self.genero = genero
+        self.faixa_etaria = faixa_etaria
+        self.disponibilidade = True
+        self.responsavel = None
+        
+    #Ver quem está responsável pelo livro
+    def ver_responsavel(self):
+        if self.responsavel == None:
+            return print('O livro está em sua responsabilidade')
+        else:
+            return print('{} está responsável pelo livro'.format(self.responsavel.nome))
+
+    #Realizar um empréstimo
+    def emprestar(self, amigo):
+        if self.disponibilidade == True and amigo.idade >= self.faixa_etaria:
+            self.responsavel = amigo.nome
+            amigo.livros.append(self.titulo)
+            self.responsavel = amigo
+            self.disponibilidade = False
+            return print('Empresatado para {}'.format(amigo.nome))
+        elif self.disponibilidade == False:
+            return print('Este livro já está empresatado para {}'.format(self.responsavel.nome))
+        elif self.faixa_etaria > amigo.idade:
+            return print('{} não tem idade para ler esse livro'.format(amigo.nome))
+        
+class Amigo:
+    def __init__(self, nome, numero, email, ano_de_nascimento):
+        self.nome = nome
+        self.numero = numero
+        self.email = email
+        self.idade = 2021 - ano_de_nascimento
+        self.livros = []
+
+    #Ver quais livros estão sob responsabilidade do amigo
+    def responsavel_livros(self):
+        if self.livros == None:
+            return print('{} está responsável por: {}'.format(self.nome, self.livros))
+        else:
+            return print('{} não está responsável por nenhum livro'.format(self.nome))
+
+#Cadastro Amigos       
+Lukas = Amigo('Lukas', 999999999, 'lukas.ristow@gmail.com', 2000)
+Vitor = Amigo('Vitor', 999999999, 'vitor.sergio@gmail.com', 2000)
+Davi = Amigo('Davi', 999999999, 'davi.gubler@gmail.com', 2004)
+
+#Cadastro Livros
+Guerra_do_Velho = Livro('Guerra do Velho', '', 'John Scalzi', 'John Perry', 'Ficção Científica', 14)
+Uzumaki = Livro('Uzumaki', '', 'Junji Ito', 'Kirie Goshima', 'Terror', 18)
+lotr = Livro('Senhor dos Anéis: A Sociedade do Anel', '', 'J. R. R. Tolkien', 'Frodo Bolseiro', 'Fantasia', 12)
+
+#Consultas
+Guerra_do_Velho.ver_responsavel()
+Guerra_do_Velho.emprestar(Lukas)
+Guerra_do_Velho.emprestar(Vitor)
+Guerra_do_Velho.ver_responsavel()
+lotr.emprestar(Lukas)
+Lukas.responsavel_livros()
+Uzumaki.emprestar(Davi)
+Davi.responsavel_livros()
