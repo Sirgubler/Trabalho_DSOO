b8782aefd9f6ca139072b8fc1f09d613cdd2e610
Matheus Gubler <matheus.gubler@gmail.com>
2021-03-22 22:13:48 -0300

    Criação tela principal

--- controle/controlador_principal.py
+++ controle/controlador_principal.py
@@ -1,8 +1,31 @@
+from limite.tela_principal import TelaPrincipal
+
 class ControladorPrincipal:
 
     def __init__(self):
-        pass
+        self.__tela_principal = TelaPrincipal()
 
     def inicializa_sistema(self):
-        print("Hello, world!")
+        self.abre_tela()
 
+    def livros(self):
+        pass
+
+    def usuarios(self):
+        pass
+        
+    def encerra_sistema(self):
+        exit(0)
+
+    def abre_tela(self):
+    
+        opcoes = {1: self.livros, 2: self.usuarios, 0: self.encerra_sistema}
+
+        while True:
+
+            opcao_escolhida = self.__tela_principal.menu_principal()
+
+            funcao_escolhida = opcoes[opcao_escolhida]
+
+            funcao_escolhida()
+            
---
+++ limite/tela_principal.py
@@ -0,0 +1,13 @@
+from controle.controlador_principal import ControladorPrincipal
+
+class TelaPrincipal():
+
+    def __init__(self):
+        pass
+
+    def menu_principal(self):
+        print("----------Menu Principal----------")
+        print("1 - Menu Livros")
+        print("2 - Menu Usuários")
+        print("0 - Sair")
+        return int(input("Selecione a opção desejada:"))
