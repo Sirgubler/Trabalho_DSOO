aa2c2e07698d6921d9be8f5371276bf43b4715e5
Otavio-PC <78768354+Otavio-PC@users.noreply.github.com>
2021-04-02 06:43:56 -0300

    Hora de implementar a tela e controlador livro

--- limite/tela_livro.py
+++ limite/tela_livro.py
@@ -9,3 +9,18 @@ class TelaLivro():
         print("2 - Menu Usuários")
         print("0 - Sair")
         return int(input("Selecione a opção desejada: "))
+
+    def cadastro_livro(self):
+        pass
+
+    def exclusao_livro(self):
+        pass
+
+    def cadastro_leitor(self):
+        pass
+
+    def pesquisa_leitor(self):
+        pass
+
+    def analises(self):
+        pass
